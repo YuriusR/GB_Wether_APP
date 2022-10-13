3b12c0655a8cf2ca434c1cefcdfaf54913e734c0
YuriusR <nelf_88@mail.ru>
2022-10-14 00:27:14 +0500

    создал кнопку

--- app/src/main/java/com/example/gb_wether_app/MainActivity.kt
+++ app/src/main/java/com/example/gb_wether_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.gb_wether_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<AppCompatButton>(R.id.button).setOnClickListener(object : OnClickListener);
+        {
+
+        })
+        
     }
 }
